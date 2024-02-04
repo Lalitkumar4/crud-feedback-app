4a2eb3d69f66e35ac1fb6cd0fa594ba8540a372d
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2024-02-04 19:31:17 +0530

    Initial commit

---
+++ src/shared/Card.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types"
+
+function Card({ children, reverse }) {
+  //   return <div className={`card ${reverse && "reverse"}`}>{children}</div>;
+
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: "#fff",
+        color: "#000",
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
+}
+
+export default Card
